abc0ac4bb98a487b3379c140ab7c00ca60b7e661
tomersiv <stk112233@gmail.com>
2021-05-30 01:36:45 +0300

    added test.c

--- user/test.c
+++ user/test.c
@@ -2,56 +2,37 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
-void 
-testall(){
-  int i = 0;
-  uint64 c = 17;
-  uint64 pointers[c];
-  printf("IN PARENT\n");
-  for (i = 0 ; i < c ; i++){
-        pointers[i] = (uint64)sbrk(4096);
-        * (char *) pointers[i] = (char) ('a' + i);
-        printf("%c\n",  * (char *) pointers[i]);
+#include "kernel/param.h"
+
+void testPageFault(){
+  int *page_addresses[MAX_PSYC_PAGES + 1];
+  int pid;
+  
+  printf("parent is now creating MAX_PSYC_PAGES + 1 new pages\n");
+  for (int i = 0 ; i < MAX_PSYC_PAGES + 1 ; i++){
+        page_addresses[i] = (int *)sbrk(4096);
+        *page_addresses[i] = i;
+        printf("parent accessed page: %d. value in page: %d\n", i, i);
   }
 
-  int pid;
   if( (pid = fork()) ==0){
-      printf("IN CHILD \n");
-      for (i = 0 ; i < c ; i++){
-        printf("letter %c\n", *(char * )pointers[i]);
+    printf("child is now accessing copied pages from parent\n");
+      for (int i = 0 ; i < MAX_PSYC_PAGES + 1 ; i++){
+        printf("child accessed page: %d. value in page: %d\n", i, *page_addresses[i]);
       }
       exit(0);
   }
-  else{
-    int status;
-    wait(&status); 
+  else
+  {
+    wait(0); 
   }
-}
-
-void
-testSecFIFO(){
-  int i = 0;
-  uint c = 14;
-  uint64 pointers[c];
-  //create all files
-  for (i = 0 ; i < c ; i++){
-        pointers[i] = (uint64)sbrk(4096);
-        * (char *) pointers[i] = (char) ('a' + i);
-        printf("%c\n", *(char * )pointers[i]);
-  }
-  //accsess only 
-  for (i = 1 ; i < c/2 ; i++){
-        printf("sec %c\n", *(char * )pointers[i]);
-  }
-  printf("third %c\n", *(char * )pointers[0]);
 }
 
 int
 main(void)
 {
   printf( "--------- test  ---------\n");
-  testall();
-//   testSecFIFO();
+  testPageFault();
   exit(0);
   return 0; 
 }
